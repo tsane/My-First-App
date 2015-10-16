940d69bdf976958fa84d5714cdc60fd890d6d7b8
Sane, Tejas <Tejas_Sane@intuit.com>
2015-10-15 22:33:43 -0700

    Android Developer site : Training - Building your First App

---
+++ app/src/main/java/com/tejassane/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,42 @@
+package com.tejassane.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Get the message from the intent
+        String message = getIntent().getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        //Create text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Set the text view as activity's layout
+        setContentView(textView);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/tejassane/myfirstapp/MyActivity.java
+++ app/src/main/java/com/tejassane/myfirstapp/MyActivity.java
@@ -1,11 +1,16 @@
 package com.tejassane.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,15 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*** Called when user clicks the button ***/
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText messageET = (EditText) findViewById(R.id.edit_message);
+        intent.putExtra(EXTRA_MESSAGE, messageET.getText().toString());
+        startActivity(intent);
+
+
     }
 }
